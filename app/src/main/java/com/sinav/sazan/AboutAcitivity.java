0829519b9508fcecc5ef8fe4d00a1d830b51d78b
metehankoc <59233358+metehankoc@users.noreply.github.com>
2021-07-04 18:17:18 +0300

    about to main button

--- app/src/main/java/com/sinav/sazan/AboutAcitivity.java
+++ app/src/main/java/com/sinav/sazan/AboutAcitivity.java
@@ -2,13 +2,25 @@ package com.sinav.sazan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutAcitivity extends AppCompatActivity {
-
+    Button aboutToMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_acitivity);
+
+        aboutToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( AboutAcitivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
